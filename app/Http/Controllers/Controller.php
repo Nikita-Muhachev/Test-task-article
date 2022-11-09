97edf0d8e372da6d58f6d41004fc8380616780b5
nm.nikita <nikitosmm@yandex.ru>
2022-11-09 17:31:11 +0500

    правки

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 /**
@@ -15,26 +14,4 @@ class Controller extends BaseController
 {
     use AuthorizesRequests;
     use DispatchesJobs;
-
-    /**
-     * @param $message
-     * @return JsonResponse
-     */
-    public function message($message)
-    {
-        return response()->json([
-            'message' => $message,
-        ]);
-    }
-
-    /**
-     * @param $data
-     * @return JsonResponse
-     */
-    public function customResponse($data)
-    {
-        return response()->json([
-            'data' => $data,
-        ]);
-    }
 }
